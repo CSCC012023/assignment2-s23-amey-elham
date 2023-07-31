b48c17910effffd77e514906d2c5d0edef264398
4meyDam1e <amey.damle@mail.utoronto.ca>
2023-07-31 15:45:35 -0400

    Added more login test

--- server/tests/userController.test.js
+++ server/tests/userController.test.js
@@ -7,23 +7,7 @@ import User from "../models/User.js";
 
 const request = supertest(app);
 
-beforeAll = async () => {
-  await mongoose.disconnect();
-  dotenv.config();
-  await mongoose.connect(process.env.MONGO_TEST_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
-
-afterAll = async () => {
-  // delete all new users
-  await User.deleteMany({});
-  await mongoose.disconnect();
-};
-
 describe("Test user routes", () => {
-  // test login route
   it("should login user", async () => {
     const email = "test@test.com";
     const password = "testPassword";
@@ -45,7 +29,62 @@ describe("Test user routes", () => {
       password: password,
     });
 
-    console.log(JSON.stringify(response.body));
+    await User.deleteOne({ email: email });
+
     expect(response.status).toBe(200);
+  });
+
+  it("should prevent unverified user login", async () => {
+    const email = "test@test.com";
+    const password = "testPassword";
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user that has been verified
+    await User.create({
+      userName: "test",
+      email: email,
+      password: hashedPassword,
+      isverified: false,
+    });
+
+    const response = await request.post("/api/users/login").send({
+      email: email,
+      password: password,
+    });
+
+    await User.deleteOne({ email: email });
+    
+    expect(response.status).toBe(403);
+    expect(response.body.message).toBe("Email not verified");
+  });
+
+  it("should prevent login when password is incorrect", async () => {
+    const email = "test@test.com";
+    const password = "testPassword";
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user that has been verified
+    await User.create({
+      userName: "test",
+      email: email,
+      password: hashedPassword,
+      isverified: true,
+    });
+
+    const response = await request.post("/api/users/login").send({
+      email: email,
+      password: "123",
+    });
+
+    await User.deleteOne({ email: email });
+    
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid credentials");
   });
 });
