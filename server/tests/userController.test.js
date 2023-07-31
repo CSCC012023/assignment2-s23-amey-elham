b6af310ea87e56f0ecc18c4d15ba6d93903f6366
4meyDam1e <amey.damle@mail.utoronto.ca>
2023-07-31 15:21:33 -0400

    Wrote first passing test

--- server/tests/userController.test.js
+++ server/tests/userController.test.js
@@ -2,13 +2,15 @@ import app from "../server.js";
 import supertest from "supertest";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
 const request = supertest(app);
 
 beforeAll = async () => {
+  await mongoose.disconnect();
   dotenv.config();
-  mongoose.connect(process.env.MONGO_TEST_URI, {
+  await mongoose.connect(process.env.MONGO_TEST_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
@@ -17,24 +19,33 @@ beforeAll = async () => {
 afterAll = async () => {
   // delete all new users
   await User.deleteMany({});
+  await mongoose.disconnect();
 };
 
 describe("Test user routes", () => {
   // test login route
   it("should login user", async () => {
+    const email = "test@test.com";
+    const password = "testPassword";
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     // create user that has been verified
-    const user = User.create({
+    await User.create({
       userName: "test",
-      email: "test@test.com",
-      password: "test",
+      email: email,
+      password: hashedPassword,
       isverified: true,
     });
 
-    const response = await request.post("api/users/login").send({
-      email: "test",
-      password: "test",
+    const response = await request.post("/api/users/login").send({
+      email: email,
+      password: password,
     });
 
+    console.log(JSON.stringify(response.body));
     expect(response.status).toBe(200);
   });
 });
